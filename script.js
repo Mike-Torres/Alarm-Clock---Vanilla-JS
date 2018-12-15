0a0c0d2c6d2eef175e8f2d6208b72d79b2d54dcc
Michael Torres <michaelj.torres@yahoo.com>
2018-12-15 15:52:01 -0500

    third commit

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 function digitalClock(){
-  let date = new Date();
+  let date = new Date();  // setting up the object
 
-  let hours = date.getHours() + "";
-  let minutes = date.getMinutes() + "";
-  let seconds = date.getSeconds() + "";
+  let hours = date.getHours() + "";   //setting up the hh
+  let minutes = date.getMinutes() + ""; //setting up the mm
+  let seconds = date.getSeconds() + ""; //setting up the ss
   let day = date.getDay();
 
     if(hours.length < 2){  // This is the make sure the format is set up /hh:mm:ss/ This sets length of clock
       hours = "0" + hours;
     }
 
-     if(minutes.length < 2){
+     if(minutes.length < 2){   //format 00:00:00
        minutes = "0" + minutes;
      }
 
       if (seconds.length < 2){
        seconds = "0" + seconds;
  }
-  let weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-  let clock = weekDays[day] + ' ' + hours + ':' + minutes + ":" + seconds;
+  let clock = hours + ':' + minutes + ":" + seconds; // weekDays[day] + ' ' +
 
+  let holy = weekDays[day];
    document.getElementById('clock').innerHTML = clock;
+   document.getElementById('holy').innerHTML = holy;
 
-  console.log(clock);
+  console.log();
 };
 
-digitalClock(clock);
+digitalClock();
 
 setInterval(digitalClock, 1000);
